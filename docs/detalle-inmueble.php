8caf58a68886945ef13c3fd985022cb372d00d64
cesarmario <38296420+cesarmario@users.noreply.github.com>
2023-03-20 14:21:21 -0300

    Detalle Inmueble

--- docs/detalle-inmueble.php
+++ docs/detalle-inmueble.php
@@ -1,6 +1,6 @@
 <?PHP
-include('php/list_inmuebles_destacados');
 include('php/det_inmueble.php');
+include('php/list_inmuebles_destacados');
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
